efd50d7658996d3d212ed2501c5ba2e2dcb2fedb
Stas <stasfuryus@gmail.com>
2024-04-22 00:56:11 +0300

    + models Product, Category

--- config/settings.py
+++ config/settings.py
@@ -125,6 +125,9 @@ STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
 
+MEDIA_URL = '/media/catalog/'
+MEDIA_ROOT = BASE_DIR / 'catalog'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
