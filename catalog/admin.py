4ced8b1062fd8267e74f4e0318d253c16e156de5
Stas <stasfuryus@gmail.com>
2024-04-22 04:27:57 +0300

    + fixtures, class Command in catalog/management/commands/fill.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,9 +4,9 @@ from catalog.models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category')
+    list_display = ('id', 'name', 'price', 'category',)
     list_filter = ('category',)
-    search_fields = ('name', 'description')
+    search_fields = ('name', 'description',)
 
 
 @admin.register(Category)
