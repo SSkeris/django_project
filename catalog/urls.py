338063da2abdf05ce616fddb45236a107e637381
Stas <stasfuryus@gmail.com>
2024-05-04 19:44:27 +0300

    task 21.1

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,11 +2,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from catalog.views import product_list, contacts, product_detail
+from catalog.views import ProductListView, contacts, ProductDetailView, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView
 
 # пути для страниц на сайте
 urlpatterns = [
-                  path('', product_list, name='product_list'),
-                  path('catalog/<int:pk>/', product_detail, name='product_detail'),
-                  path('contacts/', contacts)
+                  path('', ProductListView.as_view(), name='product_list'),
+                  path('catalog/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+                  path('contacts/', contacts),
+                  path('create/', ProductCreateView.as_view(), name='product_create'),
+                  path('<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+                  path('<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
