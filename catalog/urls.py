5b21652fd2a5e2f2a763329b5eb1cd932573ee06
Stas <stasfuryus@gmail.com>
2024-06-07 20:21:20 +0300

    task 23.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,17 +1,19 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from catalog.views import ProductListView, contacts, ProductDetailView, ProductCreateView, ProductUpdateView, \
-    ProductDeleteView, toggle_active
+    ProductDeleteView, CategoryListView, toggle_active
 
 # пути для страниц на сайте
 urlpatterns = [
                   path('', ProductListView.as_view(), name='product_list'),
-                  path('catalog/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+                  path('catalog/<int:pk>/', cache_page(180)(ProductDetailView.as_view()), name='product_detail'),
                   path('contacts/', contacts),
                   path('create/', ProductCreateView.as_view(), name='product_create'),
                   path('<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
                   path('<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
                   path('<int:pk>/active/', toggle_active, name='toggle_active'),
+                  path('categories/', cache_page(180)(CategoryListView.as_view()), name="categories_list"),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- config/settings.py
+++ config/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g2*z_n(@pz*0hs^yj_ig408pg@97#ije0e8yu-jjjn_%xye7qf'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -82,8 +82,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'catalog',
-        'USER': 'postgres',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
         'PASSWORD': os.getenv('DB_KEY'),
     }
 }
@@ -143,12 +143,21 @@ LOGIN_URL = '/users/login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = "masamunoff@yandex.ru"
-EMAIL_HOST_PASSWORD = os.getenv('DJANGO_YANDEX')
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', False) == 'True'
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', False) == 'True'
 
 SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+CACHE_ENABLED = True
+if CACHE_ENABLED:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+            'LOCATION': os.getenv('CACHE_LOCATION'),
+        }
+    }
